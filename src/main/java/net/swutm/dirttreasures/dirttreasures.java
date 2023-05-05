93794eede5b83d0bfec800004b8976aaea9d707d
SWUTM <balcknutfdsfsddfsdsf@gmail.com>
2023-05-06 01:32:47 +0300

    dirttreasures-1.2-1.19-1.19.2

---
+++ src/main/java/net/swutm/dirttreasures/dirttreasures.java
@@ -0,0 +1,35 @@
+package net.swutm.dirttreasures;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(dirttreasures.MOD_ID)
+public class dirttreasures
+{
+    // Directly reference a slf4j logger
+    public static final String MOD_ID = "dirttreasures";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public dirttreasures()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getName());
+    }
+}
